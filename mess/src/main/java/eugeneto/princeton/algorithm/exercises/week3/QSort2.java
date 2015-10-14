86db8d45fe3303914de60fd82da4ac01bfaffdb9
tochilkin <eugenetochilkin@gmail.com>
2015-10-14 10:00:11 +0300

    prog

---
+++ mess/src/main/java/eugeneto/princeton/algorithm/exercises/week3/QSort2.java
@@ -0,0 +1,39 @@
+package eugeneto.princeton.algorithm.exercises.week3;
+
+import eugeneto.princeton.algorithm.Utils;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tochilkin
+ * Date: 14.10.15
+ * Time: 10:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class QSort2 {
+    private static String str = " 63 74 77 38 58 72 18 66 44 89 91 53";
+
+    public static void main(String[] args) {
+        int[] arr = Utils.strToIntArray(str.split("\\s"));
+        int i = 1;
+        int j = arr.length - 1;
+
+        while (true) {
+            while(arr[i] < arr[0]) i++;
+            while(arr[j] > arr[0]) j--;
+            if (i > j) {
+                int e = arr[0];
+                arr[0] = arr[j];
+                arr[j] = e;
+                break;
+            }
+
+            System.out.println(i + " " + j);
+            int e = arr[i];
+            arr[i] = arr[j];
+            arr[j] = e;
+        }
+        System.out.println(Arrays.toString(arr).replaceAll(",", ""));
+    }
+}
---
+++ mess/src/main/java/eugeneto/princeton/algorithm/exercises/week3/QSort3.java
@@ -0,0 +1,42 @@
+package eugeneto.princeton.algorithm.exercises.week3;
+
+import eugeneto.princeton.algorithm.Utils;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tochilkin
+ * Date: 14.10.15
+ * Time: 10:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class QSort3 {
+    private static String str = "59 84 30 47 59 48 89 59 60 26";
+
+    public static void main(String[] args) {
+        int[] arr = Utils.strToIntArray(str.split("\\s"));
+        int lt = 0;
+        int i = 1;
+        int gt = arr.length - 1;
+
+        while (true) {
+            if (arr[i] < arr[lt]) {
+                int e = arr[i];
+                arr[i] = arr[lt];
+                arr[lt] = e;
+                lt++;
+                i++;
+            } else if (arr[i] > arr[lt]) {
+                int e = arr[i];
+                arr[i] = arr[gt];
+                arr[gt] = e;
+                gt--;
+            } else {
+                i++;
+            }
+            if (i > gt) break;
+        }
+        System.out.println(Arrays.toString(arr).replaceAll(",", ""));
+    }
+}
